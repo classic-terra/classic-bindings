82b2fd3e6bb971dcefe242c8b85db858cd928bb1
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2023-05-26 18:13:13 +0700

    change classic_bindings in bindings/examples

--- packages/bindings/examples/schema.rs
+++ packages/bindings/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use terra_bindings::{
+use classic_bindings::{
     ExchangeRateItem, ExchangeRatesResponse, SwapResponse, TaxCapResponse,
     TaxRateResponse, TerraQuery, TerraMsg
 };
